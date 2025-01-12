0ec61aa329ffc8a8cd1f6054859b953aa03a3190
alwi <alwiaqw@yahoo.com>
2025-01-13 01:57:51 +0700

    anjay

--- main.cpp
+++ main.cpp
@@ -25,3 +25,28 @@ const string METODE_PEMBAYARAN[2][3] = {
     {"1", "Qris", "1"}
 };
 
+const string LINESPACING = "==========================";
+
+void menampilkanMenuUtama() {
+    int totalMenu = sizeof(MENU_CAFE) / sizeof(MENU_CAFE[0]);
+    cout << LINESPACING << "\n";
+    for (int i = 0; i < totalMenu; i++) {
+        cout << MENU_CAFE[i][0] << " " << MENU_CAFE[i][1] << " Rp." << MENU_CAFE[i][2] << "\n";
+    }
+    cout << LINESPACING << "\n";
+}
+
+void displayHalamanAwal() {
+    int daftarPerintah = sizeof(HALAMAN_UTAMA) / sizeof(HALAMAN_UTAMA[0]);
+    cout << LINESPACING << "\n";
+    for (int i = 0; i < daftarPerintah; i++) {
+        cout << HALAMAN_UTAMA[i][0] << " " << HALAMAN_UTAMA[i][1] << "\n";
+    }
+    cout << LINESPACING << "\n";
+}
+
+int main() {
+    menampilkanMenuUtama();
+    displayHalamanAwal();
+    return 1;
+}
